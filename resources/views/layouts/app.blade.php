9f57fa3fa4d48c5a66403c795b772b1e92cfa039
Carlos Artur <cang.carlos@gmail.com>
2018-11-25 01:01:03 -0200

    Organiza rotas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,28 +6,28 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Favicon-->
-    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="../../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
 
     
     <!-- Waves Effect Css -->
-    <link href="../../plugins/node-waves/waves.css" rel="stylesheet" />
+    <link href="{{ asset('plugins/node-waves/waves.css') }}" rel="stylesheet" />
     
     <!-- Animation Css -->
-    <link href="../../plugins/animate-css/animate.css" rel="stylesheet" />
+    <link href="{{ asset('plugins/animate-css/animate.css') }}" rel="stylesheet" />
     
     <!-- Custom Css -->
-    <link href="../../css/style.css" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     
-    <link href="../../plugins/multi-select/css/multi-select.css" rel="stylesheet">
+    <link href="{{ asset('plugins/multi-select/css/multi-select.css') }}" rel="stylesheet">
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-    <link href="../../css/themes/all-themes.css" rel="stylesheet" />
+    <link href="{{ asset('css/themes/all-themes.css') }}" rel="stylesheet" />
     @stack('style')
 
 </head>
@@ -86,7 +86,7 @@
             <!-- User Info -->
             <div class="user-info">
                 <div class="image">
-                    <img src="../../images/user.png" width="48" height="48" alt="User" />
+                    <img src="{{ asset('images/user.png') }}" width="48" height="48" alt="User" />
                 </div>
                 <div class="info-container">
                     <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ \Auth::user()->name }}</div>
@@ -335,27 +335,27 @@
     </section>
 
     <!-- Jquery Core Js -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 
     <!-- Bootstrap Core Js -->
-    <script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
 
     <!-- Select Plugin Js -->
-    <script src="../../plugins/bootstrap-select/js/bootstrap-select.js"></script>
+    <script src="{{ asset('plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
 
     <!-- Slimscroll Plugin Js -->
-    <script src="../../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <script src="{{ asset('plugins/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
 
     <!-- Waves Effect Plugin Js -->
-    <script src="../../plugins/node-waves/waves.js"></script>
+    <script src="{{ asset('plugins/node-waves/waves.js') }}"></script>
     
-    <script src="../../plugins/multi-select/js/jquery.multi-select.js"></script>
+    <script src="{{ asset('plugins/multi-select/js/jquery.multi-select.js') }}"></script>
 
     <!-- Custom Js -->
-    <script src="../../js/admin.js"></script>
+    <script src="{{ asset('js/admin.js') }}"></script>
 
     <!-- Demo Js -->
-    <script src="../../js/demo.js"></script>
+    <script src="{{ asset('js/demo.js') }}"></script>
     @stack('scripts')
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -34,54 +34,69 @@ Route::middleware('auth')->middleware('role:admin')->prefix('admin')->group(func
     Route::get('/edit_home_page/{saved?}', 'HomepageController@editHomePageForm')->name('admin.homepage.editHomePageForm');
     Route::post('/edit_home_page', 'HomepageController@editHomePageEdit')->name('admin.homepage.editHomePageEdit');
 
-    Route::any('/retrieve_flavour', 'FlavourController@index')->name('admin.flavour.retrieve');
-    Route::get('/edit_flavour/{id}', 'FlavourController@edit')->name('admin.flavour.edit');
-    Route::get('/add_flavour', 'FlavourController@add')->name('admin.flavour.add');
-    Route::get('/delete_flavour/{id}', 'FlavourController@delete')->name('admin.flavour.delete');
-    Route::post('/save_flavour/{id?}', 'FlavourController@save')->name('admin.flavour.save');
-    Route::get('/get_ingredients/{id?}', 'FlavourController@getIngredients')->name('admin.flavour.get_ingredients');
+    Route::middleware('permission:crud')->prefix('flavour')->group(function () {
+        Route::any('/', 'FlavourController@index')->name('admin.flavour.retrieve');
+        Route::get('/edit/{id}', 'FlavourController@edit')->name('admin.flavour.edit');
+        Route::get('/add', 'FlavourController@add')->name('admin.flavour.add');
+        Route::get('/delete/{id}', 'FlavourController@delete')->name('admin.flavour.delete');
+        Route::post('/save/{id?}', 'FlavourController@save')->name('admin.flavour.save');
+        Route::get('/get_ingredients/{id?}', 'FlavourController@getIngredients')->name('admin.flavour.get_ingredients');
+    });
 
-    Route::any('/retrieve_ingredient', 'IngredientController@index')->name('admin.ingredient.retrieve');
-    Route::get('/edit_ingredient/{id}', 'IngredientController@edit')->name('admin.ingredient.edit');
-    Route::get('/add_ingredient', 'IngredientController@add')->name('admin.ingredient.add');
-    Route::get('/delete_ingredient/{id}', 'IngredientController@delete')->name('admin.ingredient.delete');
-    Route::post('/save_ingredient/{id?}', 'IngredientController@save')->name('admin.ingredient.save');
+    Route::middleware('permission:crud')->prefix('ingredient')->group(function () {
+        Route::any('/', 'IngredientController@index')->name('admin.ingredient.retrieve');
+        Route::get('/edit/{id}', 'IngredientController@edit')->name('admin.ingredient.edit');
+        Route::get('/add', 'IngredientController@add')->name('admin.ingredient.add');
+        Route::get('/delete/{id}', 'IngredientController@delete')->name('admin.ingredient.delete');
+        Route::post('/save/{id?}', 'IngredientController@save')->name('admin.ingredient.save');
+    });
 
-    Route::get('/retrieve_category', 'CategoryController@retrieve')->name('admin.category.retrieve');
-    Route::get('/edit_category/{id}', 'CategoryController@edit')->name('admin.category.edit');
-    Route::get('/add_category', 'CategoryController@add')->name('admin.category.add');
-    Route::get('/delete_category/{id}', 'CategoryController@delete')->name('admin.category.delete');
-    Route::get('/get_sizes_prices/{id?}', 'CategoryController@getSizesPrices')->name('admin.category.getSizesPrices');
-    Route::post('/retrieve_category', 'CategoryController@retrieve')->name('admin.category.retrieve');
-    Route::post('/save_category/{id?}', 'CategoryController@save')->name('admin.category.save');
+    Route::middleware('permission:crud')->prefix('category')->group(function () {
+        Route::get('/', 'CategoryController@retrieve')->name('admin.category.retrieve');
+        Route::get('/edit/{id}', 'CategoryController@edit')->name('admin.category.edit');
+        Route::get('/add', 'CategoryController@add')->name('admin.category.add');
+        Route::get('/delete/{id}', 'CategoryController@delete')->name('admin.category.delete');
+        Route::get('/get_sizes_prices/{id?}', 'CategoryController@getSizesPrices')->name('admin.category.getSizesPrices');
+        Route::post('/save/{id?}', 'CategoryController@save')->name('admin.category.save');
+    });
+    
+    Route::middleware('permission:crud')->prefix('size')->group(function () {
+        Route::get('/', 'SizeController@index')->name('admin.size.retrieve');
+        Route::get('/edit/{id}', 'SizeController@edit')->name('admin.size.edit');
+        Route::get('/add', 'SizeController@add')->name('admin.size.add');
+        Route::get('/delete/{id}', 'SizeController@delete')->name('admin.size.delete');
+        Route::post('/', 'SizeController@index')->name('admin.size.retrieve');
+        Route::post('/save/{id?}', 'SizeController@save')->name('admin.size.save');
+    });
 
-    Route::get('/retrieve_size', 'SizeController@index')->name('admin.size.retrieve');
-    Route::get('/edit_size/{id}', 'SizeController@edit')->name('admin.size.edit');
-    Route::get('/add_size', 'SizeController@add')->name('admin.size.add');
-    Route::get('/delete_size/{id}', 'SizeController@delete')->name('admin.size.delete');
-    Route::post('/retrieve_size', 'SizeController@index')->name('admin.size.retrieve');
-    Route::post('/save_size/{id?}', 'SizeController@save')->name('admin.size.save');
+    Route::middleware('permission:cart')->prefix('person')->group(function () {
+        Route::get('/autocomplete/{nome?}', 'OrderController@autocompletePeople')->name('admin.autocomplete_people');
+        Route::post('/new', 'OrderController@newPerson')->name('admin.new_person');
+        Route::get('/order', 'OrderController@orderPerson')->name('admin.order_person');
+    });
 
-    Route::get('/autocomplete_people/{nome?}', 'OrderController@autocompletePeople')->name('admin.autocomplete_people');
-    Route::get('/order_start', 'OrderController@startOrder')->name('admin.startOrder');
-    Route::get('/restart_order', 'OrderController@restartOrder')->name('admin.restartOrder');
-    Route::get('/cart', 'OrderController@cart')->name('admin.cart');
-    Route::get('/remove_cart_item/{id}', 'OrderController@removeCartItem')->name('admin.remove_cart_item');
-    Route::get('/order_step2/{size_id?}/{category_id?}', 'OrderController@step2')->name('admin.step2');
-    Route::post('/new_person', 'OrderController@newPerson')->name('admin.new_person');
-    Route::get('/order_person', 'OrderController@orderPerson')->name('admin.order_person');
-    Route::post('/order_step3', 'OrderController@step3')->name('admin.step3');
-    Route::post('/order_ok', 'OrderController@order_ok')->name('admin.order_ok');
-    Route::post('/order_get_new', 'OrderController@orderGetNew')->name('order.get_new_orders');
-    Route::get('/orders_list', 'OrderController@ordersList')->name('admin.orders');
+    Route::middleware('permission:cart')->prefix('order')->group(function () {
+        Route::get('/start', 'OrderController@startOrder')->name('admin.startOrder');
+        Route::get('/restart', 'OrderController@restartOrder')->name('admin.restartOrder');
+        Route::get('/step2/{size_id?}/{category_id?}', 'OrderController@step2')->name('admin.step2');
+        Route::post('/step3', 'OrderController@step3')->name('admin.step3');
+        Route::post('/ok', 'OrderController@order_ok')->name('admin.order_ok');
+        Route::post('/get_new', 'OrderController@orderGetNew')->name('order.get_new_orders');
+        Route::get('/list', 'OrderController@ordersList')->name('admin.orders');
+    });
 
+    Route::middleware('permission:cart')->prefix('cart')->group(function () {
+        Route::get('/', 'OrderController@cart')->name('admin.cart');
+        Route::get('/remove_item/{id}', 'OrderController@removeCartItem')->name('admin.remove_cart_item');
+    });
 
-    Route::get('/retrieve_combo', 'CombosController@index')->name('admin.combo.retrieve');
-    Route::get('/edit_combo/{id}', 'CombosController@edit')->name('admin.combo.edit');
-    Route::get('/add_combo', 'CombosController@create')->name('admin.combo.add');
-    Route::get('/delete_combo/{id}', 'CombosController@delete')->name('admin.combo.delete');
-    Route::post('/retrieve_combo', 'CombosController@retrieve')->name('admin.combo.retrieve');
-    Route::post('/save_combo/{id?}', 'CombosController@store')->name('admin.combo.save');
+    Route::middleware('permission:cart')->prefix('combo')->group(function () {
+        Route::get('/', 'CombosController@index')->name('admin.combo.retrieve');
+        Route::get('/edit/{id}', 'CombosController@edit')->name('admin.combo.edit');
+        Route::get('/add', 'CombosController@create')->name('admin.combo.add');
+        Route::get('/delete/{id}', 'CombosController@delete')->name('admin.combo.delete');
+        Route::post('/save/{id?}', 'CombosController@store')->name('admin.combo.save');
+    });
 });
 
 // Route::get('images/{filename}', function ($filename) {
